50cf65db97b23d52afa8f8f0ebf13fad5ae73ca4
gdamss <elbryan@hotmail.fr>
2025-02-25 07:03:19 +0100

    suit cours dyma

---
+++ src/35_methode_disponible_caractere.js
@@ -0,0 +1,13 @@
+const a = "soleil,terre,lune";
+
+const b = a.endsWith("suis");
+const c = a.startsWith("suis");
+const d = a.indexOf("suis , 5");
+const e = a.replace("suis ", "ne suis pas");
+const f = a.searchsearch("suis");
+const g = a.slice(0, a.length);
+const h = a.trim();
+const i = a.split(",");
+
+
+console.log(i);
---
+++ src/36_experssions_rationnelles.js
@@ -0,0 +1,5 @@
+const a = "maphoto_12451.jpg";
+
+console.log(a.replaceAll(/\d/g, "a"));
+
+console.log(a);
